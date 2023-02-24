f2c6719ed623589c4c0ecf0c8f7d70e540907673
Lara-Vel <lara.vel@outlook.com>
2023-02-24 12:11:38 +0100

    remove default storage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,9 @@
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  render(<App />);
 
   expect(true).toBe(true);
 });
