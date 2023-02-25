311fbb7f9f6a1bf289b7a6f77890cd7611a3e15c
Lara-Vel <lara.vel@outlook.com>
2023-02-25 16:58:56 +0100

    add robotcard to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css';
+import RobotCard from './features/robotCard/RobotCard';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <RobotCard
+        robot={{
+          id: 0,
+          img: 'string',
+          name: 'string',
+          speed: 0,
+          resistance: 0,
+          dateOfCreation: '',
+        }}
+      />
     </div>
   );
 }
